9e1c13e0de794dc1e10a7e7a4b3b7c64035fde48
kimjangsoo <quakemysty@gmail.com>
2026-09-12 18:35:03 +0900

    불필요한 구문 정리

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,12 @@
 //-----------------------------------
 // for chrome extension
 //-----------------------------------
-import { defineConfig, type Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
-//import manifest from "./public/manifest.json";
-import manifest from "./manifest.config";
-
-function watchJson(paths: string[]): Plugin {
-    return {
-        name: "watch-json",
-        configureServer(server) {
-            server.watcher.add(paths);
-            server.watcher.on("change", (file) => {
-                if (!file.endsWith(".json")) return;
-                server.ws.send({ type: "custom", event: "ext:json-changed", data: { file } });
-                server.config.logger.info(`[watch-json] ${file} reload`);
-            });
-        },
-    };
-}
+import manifest from "./manifest.config.ts";
 
 export default defineConfig({
-    plugins: [react(), crx({ manifest }), watchJson(["src/locales/*.json", "src/assets/*.json"])],
+    plugins: [react(), crx({ manifest })],
     base: "./",
-    server: {
-        port: 5173,
-        strictPort: true,
-        hmr: { port: 5173 },
-    },
 });
