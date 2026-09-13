f75ab5f4751452a63b92e98e1e9c9e18a790849c
kimjangsoo <quakemysty@gmail.com>
2026-09-14 00:11:20 +0900

    1.0.0.2 로 버전 수정

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
     manifest_version: 3,
     name: "POE Simple Trade",
-    version: "1.0.0.1",
+    version: "1.0.0.2",
     description: "POE1,2 Simple Bookmark for Trading",
     permissions: ["storage", "clipboardWrite"],
     background: { service_worker: "src/background/background.ts", type: "module" },
